723d6d94fcc33cee2cce77b67025b2dc719c1955
LavenderPeach <134173296+LavenderPeach@users.noreply.github.com>
2024-01-17 17:40:38 -0500

    wrote a soup method to find all player names on the html file based on attributes the preceded each instance of a player name.
    created a set for encountered player names so that I would not pull duplicate names (on this page, every player is listed twice)
    
    create a for loop that goes throgh all the players who are discovered via the find method and strips all the text around them, leaving only their html text (in this instance, their names)
    
    append names to the list with an if statement that adds player names and links (to get personal information about each later on) to the encountered_names set.

--- script.py
+++ script.py
@@ -2,24 +2,44 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from bs4 import BeautifulSoup
 
-# Set the path to your Edge WebDriver executable
+# Set path to webdriver
 driver_path = r'C:\Users\cadeg\Desktop\chromedriver.exe'
 
-# Create a Microsoft Edge webdriver instance
-edge_service = ChromeService(driver_path)
-driver = webdriver.Chrome(service=edge_service)
+# chrome webdriver instance
+chrome_service = ChromeService(driver_path)
+driver = webdriver.Chrome(service=chrome_service)
 
-# Open NBA.com team stats page for the Boston Celtics
+# open nba.com team stats page
 team_stats_url = 'https://www.espn.com/nba/team/stats/_/name/bos'
 driver.get(team_stats_url)
 
 driver.implicitly_wait(10)
-# Get the updated HTML content after JavaScript execution
+
+
+# get html content once javascript has executed
 html_content = driver.page_source
 
-# Close the browser
+# close browser
 driver.quit()
 
-# Parse HTML content using BeautifulSoup
+# parse html with beautifulsoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
+# find all player name links
+player_links = soup.find_all('a', class_='AnchorLink', attrs={'href': True, 'data-player-uid': True} )
+
+# create set to prevent duplicate names
+encountered_names = set()
+
+for player_link in player_links:
+    player_name = player_link.text.strip()
+    
+    # Check if the player name has not been encountered before
+    if player_name not in encountered_names:
+        player_url = player_link['href']
+        print("Player:", player_name)
+        print("Player URL:", player_url)
+        print("-------------------")
+        
+        # Add the name to the set to avoid printing it again
+        encountered_names.add(player_name)
