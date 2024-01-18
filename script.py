9fa3085f45f5812559e3586c2e8a1c1cbff68c20
LavenderPeach <134173296+LavenderPeach@users.noreply.github.com>
2024-01-18 16:32:08 -0500

    added line that activates the soup find method to retrive all table rows from the html.
    nested a for loop inside of an if statement to run a program where if there are any rows, then data cells will be pulled from that row. Tables will then be pulled based on how many data cells are in the row.
    
    variables within the if statement is the data being pulled. I assigned a data cell at each index a specific variable and then stripped the html around the text of the cell so I could pull the necessary information.

--- script.py
+++ script.py
@@ -43,3 +43,54 @@ for player_link in player_links:
         
         # Add the name to the set to avoid printing it again
         encountered_names.add(player_name)
+
+# get all rows
+
+rows = soup.find_all('tr', class_ = 'Table__TR')
+
+if rows:
+    # pull values out of rows
+    for row in rows:
+        data_cells = row.find_all('td')
+        if len(data_cells) == 13:
+            games_played = data_cells[0].text.strip()
+            games_started = data_cells[1].text.strip()
+            minutes = data_cells[2].text.strip()
+            points = data_cells[3].text.strip()
+            o_rebounds = data_cells[4].text.strip()
+            d_rebounds = data_cells[5].text.strip()
+            rebounds = data_cells[6].text.strip()
+            assists = data_cells[7].text.strip()
+            steals = data_cells[8].text.strip()
+            blocks = data_cells[9].text.strip()
+            turnovers = data_cells[10].text.strip()
+            fouls = data_cells[11].text.strip()
+            assist_turnover_ratio = data_cells[12].text.strip()
+
+            print("Points:", points)
+            print("Rebounds:", rebounds)
+            print("Assists:", assists)
+            print("-------------------")
+        elif len(data_cells) == 14:
+            fg_made = data_cells[0].text.strip()
+            fg_attempt = data_cells[1].text.strip()
+            fg_percent = data_cells[2].text.strip()
+            three_made = data_cells[3].text.strip()
+            three_attempt = data_cells[4].text.strip()
+            three_percent = data_cells[5].text.strip()
+            ft_made = data_cells[6].text.strip()
+            ft_attempt = data_cells[7].text.strip()
+            ft_percent = data_cells[8].text.strip()
+            two_made = data_cells[9].text.strip()
+            two_attempt = data_cells[10].text.strip()
+            two_percent = data_cells[11].text.strip()
+            scoring_efficiency = data_cells[12].text.strip()
+            shooting_efficiency = data_cells[13].text.strip()
+
+            print("FGM:", fg_made)
+            print("SH-EFF:", shooting_efficiency)
+        else:
+            print("Not enough data cells for this row")
+
+else:
+    print("No table rows found on the webpage.")
